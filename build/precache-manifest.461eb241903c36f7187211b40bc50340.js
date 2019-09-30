cfe64d50abcab0fdaec6c75e20505724cdac83eb
Mike Ivonin <Michael@drummer.ru>
2019-09-30 13:28:48 +0600

    Early build

---
+++ build/precache-manifest.461eb241903c36f7187211b40bc50340.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e0555347911cd1572c134db891dcaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e96226c6e52616f55d23",
+    "url": "/static/css/2.e2043e69.chunk.css"
+  },
+  {
+    "revision": "6cb8a96b7088f505583e",
+    "url": "/static/css/main.80df92a2.chunk.css"
+  },
+  {
+    "revision": "e96226c6e52616f55d23",
+    "url": "/static/js/2.55f35667.chunk.js"
+  },
+  {
+    "revision": "6cb8a96b7088f505583e",
+    "url": "/static/js/main.3c137f36.chunk.js"
+  },
+  {
+    "revision": "6614294f3ffeb8251735",
+    "url": "/static/js/runtime-main.53b375bc.js"
+  }
+]);
